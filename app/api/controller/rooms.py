e8f8ae7dcd403497d7863b568e86fccd67460321
JasurShermatov <shermatovjasur800@gmail.com>
2024-12-19 11:06:47 +0500

    ..
    ..

--- app/api/controller/rooms.py
+++ app/api/controller/rooms.py
@@ -1,11 +1,17 @@
 from fastapi import Depends
+
 from app.api.repositories.rooms import RoomsRepository
+
 
 class RoomsController:
     def __init__(
             self,
-            rooms_repo: RoomsRepository=Depends()
+            rooms_repo: RoomsRepository = Depends()
     ):
         self.__rooms_repo = rooms_repo
-        async def get_rooms():
-            pass
+
+    async def get_rooms(self):
+        return await self.__rooms_repo.get_all_rooms()
+
+    async def get_room(self, room_id):
+        return await self.__rooms_repo.get_room(room_id)
--- app/api/views/rooms.py
+++ app/api/views/rooms.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter, Depends
 
-from app.api import controller
 from app.api.controller.rooms import RoomsController
 
 router = APIRouter()
 
 
 @router.get("/")
-async def rooms(
-        conntroller:RoomsController=Depends()
+async def get_rooms(
+        controller: RoomsController = Depends()
 ):
     return await controller.get_rooms()
 
+@router.get("/{room_id}")
+async def get_room(
+        controller: RoomsController = Depends(),
+        room_id: int | None = None
+):
+    return await controller.get_room(room_id)
